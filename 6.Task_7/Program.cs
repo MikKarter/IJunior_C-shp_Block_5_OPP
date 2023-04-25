21f691a447d73f3d1fbb1c18115a380af3363424
MikKarter <66348029+MikKarter@users.noreply.github.com>
2023-04-25 17:35:56 +0900

    Step_1

--- 6.Task_7/Program.cs
+++ 6.Task_7/Program.cs
@@ -12,4 +12,39 @@ namespace _6.Task_7
         {
         }
     }
+
+    class RaliwayCarriage
+    {
+        public int NumberOfPassengers { get; private set; }
+        public int MaxPlace{ get; private set; }
+        public int FreePlace { get; private set; }
+
+        public RaliwayCarriage (int numberOfPassengers, int maxPlace, int freePlace)
+        {
+            NumberOfPassengers = numberOfPassengers;
+            MaxPlace = maxPlace;
+            FreePlace = maxPlace - numberOfPassengers;
+        }
+    }
+
+    class TicketTrade
+    {
+        private Random _random = new Random();
+        private int _ticketSoldMin = 0;
+        private int _ticketSoldMax = 51;
+        private int _countSoldTicket;
+
+        public int Sell()
+        {
+            _countSoldTicket = _random.Next(_ticketSoldMin, _ticketSoldMax);
+            Console.WriteLine($"Продано {_countSoldTicket} билетов");
+            return _countSoldTicket;
+        }
+    }
+
+    class Train
+    {        
+        public List<RaliwayCarriage> RaliwayCarriages { get; private set; }
+        public Dictionary<string, int> Trains { get; private set; }
+    }
 }
